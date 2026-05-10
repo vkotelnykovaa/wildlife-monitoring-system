847d69512e3b6dcc8c622144c7a38a1888aefc6f
vkotelnykovaa <lerkotelnikova6@gmail.com>
2026-05-10 11:57:44 +0300

    initial diploma project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
